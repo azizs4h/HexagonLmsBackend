068c8c3ec049c1a6422c620b765a6f950048c9b7
Ömer Aziz ŞAHİN <omarazizsah@gmail.com>
2021-04-13 02:42:05 +0300

    db pk hatası kontrol edildi

--- student/models.py
+++ student/models.py
@@ -5,7 +5,6 @@ from user.models import User
 
 class Student(models.Model):
     student_user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_user')
-    student_no = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     email = models.EmailField(max_length=255)
